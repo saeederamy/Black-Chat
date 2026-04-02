1bf35da197b1a19ff53f501e32c729aba8d3a7f8
saeederamy <saeederamy@gmail.com>
2026-04-02 22:16:29 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,15 @@
 WORKING_DIR="/opt/black-chat"
 mkdir -p "$WORKING_DIR"
 
-echo "[+] Downloading Management Script..."
-# لینک زیر را با آدرس فایل manage_chat.sh در گیت‌هاب خودت عوض کن
+echo -e "\033[0;36m[+] Downloading Management Script...\033[0m"
 curl -sL "https://raw.githubusercontent.com/saeederamy/Black-Chat/main/manage_chat.sh" -o "$WORKING_DIR/manage_chat.sh"
 chmod +x "$WORKING_DIR/manage_chat.sh"
 
 sudo ln -sf "$WORKING_DIR/manage_chat.sh" /usr/local/bin/black-chat
 sudo chmod +x /usr/local/bin/black-chat
 
-echo -e "\033[0;32m[✔] Installation complete! Type 'black-chat' to open the menu.\033[0m"
+echo -e "\033[0;32m[✔] Installation script loaded successfully!\033[0m"
+sleep 1
+
+# باز کردن خودکار منو
+black-chat
