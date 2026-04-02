4236b0efbaf9aa988c5a0d2e2b3a658230ab0ba4
saeederamy <saeederamy@gmail.com>
2026-04-02 22:05:37 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# install.sh
+
+WORKING_DIR="/opt/black-chat"
+mkdir -p "$WORKING_DIR"
+
+echo "[+] Downloading Management Script..."
+# لینک زیر را با آدرس فایل manage_chat.sh در گیت‌هاب خودت عوض کن
+curl -sL "https://raw.githubusercontent.com/YOUR_GITHUB/REPO/main/manage_chat.sh" -o "$WORKING_DIR/manage_chat.sh"
+chmod +x "$WORKING_DIR/manage_chat.sh"
+
+sudo ln -sf "$WORKING_DIR/manage_chat.sh" /usr/local/bin/black-chat
+sudo chmod +x /usr/local/bin/black-chat
+
+echo -e "\033[0;32m[✔] Installation complete! Type 'black-chat' to open the menu.\033[0m"
